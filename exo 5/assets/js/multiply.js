50785984e6d9adb7db447a0a7066066e5fcd419b
Illensky <alexis.laroche.02240@gmail.com>
2021-11-15 14:21:16 +0100

    Exercice done

---
+++ exo 5/assets/js/multiply.js
@@ -0,0 +1,4 @@
+let x = prompt("donnez moi c'est le donner dieu vous le rendra un nombre a virgule :")
+let y = prompt("donnez moi c'est le donner dieu vous le rendra un deuxieme nombre a virgule :")
+
+alert(parseInt(x)*y)
---
+++ exo 6/assets/js/multiply.js
@@ -0,0 +1,6 @@
+let x = prompt("Quel est votre age?")
+
+if (x >= 18)
+    alert("Vous êtes majeur");
+else
+    alert("Vous êtes mineur");
